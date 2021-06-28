9db382bdb608fd6e351df4a66d4a07aa8e8b3f7d
Umidullo Suyunov <umidullo@suyunov.uz>
2021-06-29 00:49:31 +0500

    feat: add cards

--- english-for-kids-base/src/components/Main/index.js
+++ english-for-kids-base/src/components/Main/index.js
@@ -1,8 +1,18 @@
 import './style.css'
+import Card from './Card'
 
 const Main = () => {
   return (
     <main className="main">
+      <div className="cards-field">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+
+      </div>
     </main>
   )
 }
